01e13a3fceb7796ac1c0fdc519730d445884301a
onosaito <onosaito0614@gmail.com>
2022-02-07 22:18:26 +0900

    Revert "modified usermodel"
    This reverts commit 4508c3b97cf86c84f4cee785ac0d36e91b13df45.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
@@ -6,43 +7,33 @@ class CustomUserManager(BaseUserManager):
 	
 	use_in_migrations = True
 
-	def _create_user(self, username, email, **extra_fields):
-		if not email:
+	def _create_user(self, request_data, **extra_fields):
+		if not request_data['email']:
 			raise ValueError('emailを入力してください')
-		if not username:
-			raise ValueError('usernameを入力してください')
-		email = self.normalize_email(email)
-		user = self.model(username=username, email=email, **extra_fields)
+		email = self.normalize_email(request_data['email'])
+		user = self.model(username=request_data['username'], email=email, **extra_fields)
+		user.set_password(request_data['password'])
 		user.save(using=self.db)
 		return user
 
-	def create_user(self, username, email, **extra_fields):
+	def create_user(self, request_data, **extra_fields):
 		extra_fields.setdefault('is_staff', False)
 		extra_fields.setdefault('is_superuser', False)
-		return self._create_user(username, email, **extra_fields)
+		return self._create_user(request_data, **extra_fields)
 	
-	def create_superuser(self, username, email, password, **extra_fields):
+	def create_superuser(self, request_data, **extra_fields):
 		extra_fields.setdefault('is_staff', True)
 		extra_fields.setdefault('is_superuser', True)
 		if extra_fields.get('is_staff') is not True:
-			raise ValueError('staffがTrueではないです')
+			raise ValueError('staff=Falseになっています')
 		if extra_fields.get('is_superuser') is not True:
-			raise ValueError('is_superuserがTrueではないです')
-		if not email:
-			raise ValueError('emailを入力してください')
-		if not username:
-			raise ValueError('usernameを入力してください')
-		email = self.normalize_email(email)
-		user = self.model(username=username, email=email, **extra_fields)
-		user.set_password(password)
-		user.save(using=self.db)
-		return user
+			raise ValueError('is_superuser=Falseになっています')
+		return self._create_user(request_data, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-	email = models.EmailField('email', unique=True, null=True)
+	email = models.EmailField('email', null=True)
 	username = models.CharField('username', unique=True, max_length=150)
-	image_url = models.CharField('imageUrl', null=True, max_length=500)
 	is_staff = models.BooleanField('is_staff', default=False)
 	is_active = models.BooleanField('is_active', default=True)
 	date_joined = models.DateTimeField('date_joined', default=timezone.now)
